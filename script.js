52ccd00a9d305931da24e959b52042c41faeae3b
NeerajJanardanan <neerajjanardanan@gmail.com>
2023-04-17 11:44:12 +0530

    calculator add

---
+++ script.js
@@ -0,0 +1,19 @@
+function displayNumber(num){
+    // let resultBox=document.querySelector("#id_result")
+    // resultBox.value+=num
+    id_result.value+=num
+}
+
+function clearBox(){
+    id_result.value=""
+}
+function evaluatedExpression(){
+    let currentExpression=id_result.value
+    let out=eval(currentExpression)
+    id_result.value=out
+}
+function backSpace(){
+    let currentValue=id_result.value
+    let out=currentValue.slice(0,-1)
+    id_result.value=out
+}
